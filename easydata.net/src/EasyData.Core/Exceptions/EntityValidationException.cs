5018562cc50f363a159ba7e310e7cb367cc0c8fe
Vasily Chefonov <81791194+vasiliy-chefonov@users.noreply.github.com>
2021-10-26 15:38:48 +0300

    Implement custom validation exception

---
+++ easydata.net/src/EasyData.Core/Exceptions/EntityValidationException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyData.Exceptions
+{
+    /// <summary>
+    /// Custom entity validation exception.
+    /// </summary>
+    public class EntityValidationException: Exception
+    {
+        public EntityValidationException()
+        {
+
+        }
+
+        public EntityValidationException(string message): base(message)
+        {
+
+        }
+
+        public EntityValidationException(string message, Exception inner): base(message, inner)
+        {
+
+        }
+    }
+}
